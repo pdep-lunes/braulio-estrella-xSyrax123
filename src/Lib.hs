0b849106b74ac922b4ea32e914b592a3db2c0dc1
xSyrax123 <99150043+xSyrax123@users.noreply.github.com>
2025-04-28 12:20:26 -0300

    Agrego funcion bolaEspinosa

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib () where
 
 import Text.Show.Functions ()
+restarMilDeVida :: Personaje -> Int
+restarMilDeVida unPersonaje = cantidadDeVida unPersonaje - 1000
 
-doble :: Int -> Int
-doble x = x * 2
+data Personaje = UnPersonaje {
+   nombre :: String,
+   poderBasico :: String,
+   superPoder :: String,
+   tieneSuperPoderActivo :: Bool,
+   cantidadDeVida :: Int,
+}
+
+espina :: Personaje 
+espina = UnPersonaje "Espina" "bola de espinas" "granada de espinas" True 4800 
+
+pamela :: Personaje
+pamela = UnPersonaje "Pamela" "lluvia de tuercas" "torreta curativa" False 9600 
+
+bolaEspinosa :: Personaje -> Personaje
+bolaEspinosa unPersonaje = UnPersonaje {cantidadDeVida = restarMilDeVida(unPersonaje)}
